a9c67d7eaae50ed6cce208e2e630eee45dc9858a
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-29 17:04:29 +0100

    Add views

---
+++ alx_travel_app_0x01/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Travel API",
+      default_version='v1',
+      description="API for managing listings and bookings",
+      terms_of_service="https://www.google.com/policies/terms/",
+   ),
+   public=True,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
